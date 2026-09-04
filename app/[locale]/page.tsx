5386b3d3e004aa4fe0775e7f6a622a2f8764855c
samialaoufi1408-cmd <samialaoufi1408@gmail.com>
2026-09-04 18:01:15 +0300

    Render DAHSHA as fifth selected work

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import { Locale, locales } from '@/content/site';
 import { StudioHomeV4 } from '@/components/sections/StudioHomeV4';
 import { SumraWorkInjector } from '@/components/sections/SumraWorkInjector';
 import { SumraWorkCardImageFix } from '@/components/sections/SumraWorkCardImageFix';
+import { DahshaWorkInjector } from '@/components/sections/DahshaWorkInjector';
 import { WorkGridDeduper } from '@/components/sections/WorkGridDeduper';
 import layout from './home-v4-overrides.module.css';
 
@@ -17,6 +18,7 @@ export default function Page({ params }: { params: { locale: string } }) {
       <StudioHomeV4 locale={locale} />
       <SumraWorkInjector locale={locale} />
       <SumraWorkCardImageFix />
+      <DahshaWorkInjector locale={locale} />
       <WorkGridDeduper locale={locale} />
     </div>
   );
